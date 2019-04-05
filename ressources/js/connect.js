c33397070a79c37e40d0ceee911d35be1d760fe6
cecile <peigne.cecile@free.fr>
2019-04-05 11:22:02 +0200

    add modal login window

--- ressources/js/connect.js
+++ ressources/js/connect.js
@@ -6,10 +6,51 @@ const admin = document.getElementById('admin');
 const adm = ('<a>Admin</a><ul><li><a href="#">Articles</a><ul><li><a href="#">Membres</a><ul><li><a href="#">stats</a><ul><li><a href="#">Item</a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li></ul></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li></ul></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li></ul></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li></ul>');
 localStorage.setItem('adminPanel', adm);
 
+// Modal 
+// ####################################################
+let modal = null;
 
-// Connection button
+const openModal = function(e){
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+}
+
+const closeModal = function (e){
+    if (modal === null) return;
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = function (e){
+    e.stopPropagation();
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal);
+})
+
+window.addEventListener('keydown', function(e){
+    if (e.key === "Escape" || e.key === "Esc"){
+        closeModal(e);
+    } 
+    
+})
+// connection button
 const connect = document.getElementById('connect');
-const linkCo = document.getElementById('linkCo');
+
 
 // Disconnection buttons hidden at start
 const disconnect = document.getElementById('deco');
@@ -25,14 +66,22 @@ if (session === 'true'){
 }
 
 // events
-connect.onclick = co;
+// connect.onclick = co;
+
 disconnect.onclick = disc;
 
 // Connect function
-function co() {
-    let mdp = prompt('entrez votre mot de passe');
-    if (mdp === 'Castaner') {
-        linkCo.removeAttribute('href');
+
+//const linkCo = document.getElementById('linkCo');
+const logForm = document.querySelector('#login-form');
+logForm.addEventListener('submit', co)
+
+ function co(e) {
+//     let mdp = prompt('entrez votre mot de passe');
+let mdp = document.getElementById('pass');
+    
+    if (mdp.value === 'Castaner') {
+        //linkCo.removeAttribute('href');
         connect.style.display = 'none';
         admin.style.display = 'inline-block';
 
@@ -44,9 +93,14 @@ function co() {
             jQuery('.stellarnav').stellarNav();
         });
         sessionStorage.setItem('connect', true);
+
+        //Close modal
+        closeModal();
+
     } else {
         alert('Mdp invalide');
     }
+    e.preventDefault();
 }
 
 // Disconnect function
